4193c0114dfafe6be88d68f6b9a22f072089330d
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2024-09-15 17:01:35 +0530

    Update Main.java

--- src/com/mortgageCalculator/Main.java
+++ src/com/mortgageCalculator/Main.java
@@ -1,7 +1,33 @@
 package com.mortgageCalculator;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PRESENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        float principal = scanner.nextFloat();
+
+        System.out.print("Annual interest: ");
+        float annualInterest = scanner.nextFloat();
+
+        System.out.print("Period (Y): ");
+        byte period = scanner.nextByte();
+
+        // calculation
+        float monthlyInterestRate = annualInterest / (MONTHS_IN_YEAR * PRESENT);
+        int months = period * MONTHS_IN_YEAR;
+        double mortgage = (principal * monthlyInterestRate * Math.pow(monthlyInterestRate + 1, months))
+                / (Math.pow(monthlyInterestRate + 1, months) - 1);
+
+        // print result
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
     }
 }
