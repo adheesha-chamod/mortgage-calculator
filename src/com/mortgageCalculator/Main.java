b4469f77105f900b92931acd173d4c3924e72f5c
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2024-09-16 15:40:33 +0530

    Update Main.java

--- src/com/mortgageCalculator/Main.java
+++ src/com/mortgageCalculator/Main.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 
 public class Main {
 
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PRESENT = 100;
+
     public static void main(String[] args) {
         float principal = (float) readValue("Principal:", 1_000, 1_000_000);
         float annualInterest = (float) readValue("Annual interest:", 1, 30);
         byte period = (byte) readValue("Period (Y):", 1, 30);
 
-        float mortgage = calculateMortgage(principal, annualInterest, period);
-
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: " + mortgageFormatted);
+        printMortgage(principal, annualInterest, period);
+        printPaymentSchedule(principal, annualInterest, period);
     }
 
     public static double readValue(String prompt, float min, float max) {
@@ -33,13 +34,34 @@ public class Main {
     }
 
     public static float calculateMortgage(float principal, float annualInterest, byte period) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PRESENT = 100;
-
         float monthlyInterestRate = annualInterest / (MONTHS_IN_YEAR * PRESENT);
         short months = (short) (period * MONTHS_IN_YEAR);
 
-        return (float) ((principal * monthlyInterestRate * Math.pow(monthlyInterestRate + 1, months))
+        return (float) (principal
+                * (monthlyInterestRate * Math.pow(monthlyInterestRate + 1, months))
                 / (Math.pow(monthlyInterestRate + 1, months) - 1));
+    }
+
+    public static float calculateLoanBalance(float principal, float annualInterest, byte period, short noOfPaymentsMade) {
+        float monthlyInterestRate = annualInterest / (MONTHS_IN_YEAR * PRESENT);
+        short months = (short) (period * MONTHS_IN_YEAR);
+
+        return (float) (principal
+                * (Math.pow(1 + monthlyInterestRate, months) - Math.pow(1 + monthlyInterestRate, noOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterestRate, months) - 1));
+    }
+
+    private static void printMortgage(float principal, float annualInterest, byte period) {
+        System.out.println("MORTGAGE\n--------");
+        System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance()
+                .format(calculateMortgage(principal, annualInterest, period)));
+    }
+
+    private static void printPaymentSchedule(float principal, float annualInterest, byte period) {
+        System.out.println("\nPAYMENT SCHEDULE\n----------------");
+        for (short month = 1; month <= period * MONTHS_IN_YEAR; month++) {
+            System.out.println(NumberFormat.getCurrencyInstance()
+                    .format(calculateLoanBalance(principal, annualInterest, period, month)));
+        }
     }
 }
